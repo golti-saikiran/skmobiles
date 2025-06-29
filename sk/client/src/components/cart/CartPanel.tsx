3ba21be67017b1a2255d922f17a5180ae735162b
golti-saikiran <118980975+golti-saikiran@users.noreply.github.com>
2025-06-29 17:09:27 +0530

    Update CartPanel.tsx

--- sk/client/src/components/cart/CartPanel.tsx
+++ sk/client/src/components/cart/CartPanel.tsx
@@ -23,9 +23,6 @@ export const CartPanel: React.FunctionComponent = () => {
     deepSkyBlue: [{ color: "black" }, iconClass],
   });
 
-const closePanel= () =>{
-  // isOpen:dismissPanel
-}
 
   return (
     <div>
