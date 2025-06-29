17ff3c741deb0bb141593075225118b3fa228a23
golti-saikiran <118980975+golti-saikiran@users.noreply.github.com>
2025-06-29 17:10:04 +0530

    Update Home.tsx

--- sk/client/src/components/Home/Home.tsx
+++ sk/client/src/components/Home/Home.tsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
     
-      <img className='homeimg' src='./Assets/homeScreen.png' width="80%" height="300px" />
+      <img className='homeimg' src='./Assets/homeScreen.png' width="80%" height="300px" alt="home-banner"/>
       <h1 className='tagline'> Find the Best Smart Mobile that fits to You</h1>
     </div>
   )
